603a0aa5d96aa53a67b3a8186934c85c98da4c29
kittvnebluda <kittnablude@gmail.com>
2023-03-01 23:10:40 +0300

    Изменено создание Person'ы из пользовательского ввода

--- src/main/kotlin/Coordinates.kt
+++ src/main/kotlin/Coordinates.kt
@@ -1,9 +1,9 @@
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Coordinates(private val x: Float, private val y: Double) {
+data class Coordinates(val x: Float, val y: Double) {
     init {
         if (x <= -948) throw InvalidUserInputException("x координата должна быть больше -948")
-        if (y > 453) throw InvalidUserInputException("y координата должна быть меньше 453")
+        if (y > 453) throw InvalidUserInputException("y координата должна быть меньше либо равна 453")
     }
 }
--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -14,30 +14,6 @@ data class Person(private val name: String,
                   private val weight: Int,
                   private val hairColor: Color,
                   private val location: Location) {
-
-    companion object {
-        fun personFromStrings(args: Array<String>): Person {
-            val strCoordinates = cc.splitSpaces(args[1])
-            if (strCoordinates.size < 2) throw InvalidUserInputException("Неправильные координаты: Нужно ввести два числа через пробел")
-            val coordinates = Coordinates(strCoordinates[0].toFloat(), strCoordinates[1].toDouble())
-
-            val height = args[2].trim().toLong()
-
-            val birthday = ZonedDateTime.parse(args[3].trim()) ?:
-            throw InvalidUserInputException("Неправильный формат дня рождения, пример: \"2011-12-03T10:15:30+01:00[Europe/Paris]\"")
-
-            val weight = args[4].trim().toInt()
-
-            val hairColor = Color.valueOf(args[5].trim().uppercase())
-
-            val strLocation = cc.splitSpaces(args[6])
-            if (strLocation.size < 3) throw InvalidUserInputException("Неправильное местонахождение: Нужно ввести три числа через пробел")
-            val locationName = if (strLocation.size > 3) strLocation[3] else null
-            val location = Location(strLocation[0].toFloat(), strLocation[1].toFloat(), strLocation[2].toInt(), locationName)
-
-            return Person(args[0], coordinates, height, birthday, weight, hairColor, location)
-        }
-    }
     init {
         if (height < 1) throw InvalidUserInputException("Высота человека должна быть больше 0")
         if (weight < 1) throw InvalidUserInputException("Вес человека должен быть больше 0")

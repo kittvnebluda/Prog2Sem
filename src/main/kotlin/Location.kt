8e045c26440b82caa70a7459b10c77ac41578749
kittvnebluda <111051246+kittvnebluda@users.noreply.github.com>
2023-03-06 22:33:55 +0300

    Delete src directory
    Удалил ненужную папку

--- src/main/kotlin/Location.kt
+++
@@ -1,5 +0,0 @@
-class Location {
-    private val x: Long? = null //Поле не может быть null
-    private val y: Float? = null //Поле не может быть null
-    private val name: String? = null //Строка не может быть пустой, Поле не может быть null
-}

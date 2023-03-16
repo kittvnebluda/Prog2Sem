81a527ca566a15016b60ce9ff0e2dd9ec46c9fd5
Hamfermen <75379938+Hamfermen@users.noreply.github.com>
2023-03-16 08:52:39 +0300

    Delete app/src/main/java/com/itmo/itmoproject2023 directory

--- app/src/main/java/com/itmo/itmoproject2023/Location.kt
+++
@@ -1,9 +0,0 @@
-package  com.itmo.itmoproject2023
-
-@kotlinx.serialization.Serializable
-data class Location (
-    var x:Float = 0f, //Поле не может быть null
-    var y:Float = 0f, //Поле не может быть null
-    var z:Int = 0, //Поле не может быть null
-    var name:String? = null //Строка не может быть пустой, Поле не может быть null
-)

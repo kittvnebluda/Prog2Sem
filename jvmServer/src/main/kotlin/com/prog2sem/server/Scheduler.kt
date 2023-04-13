006cbac9e2b0b7a98ff14296ccfeb75d7f7efd6b
Hamferman <kalash0410@gmail.com>
2023-04-13 15:44:00 +0300

    Обработка ошибки

--- jvmServer/src/main/kotlin/com/prog2sem/server/Scheduler.kt
+++ jvmServer/src/main/kotlin/com/prog2sem/server/Scheduler.kt
@@ -1,5 +1,8 @@
 package com.prog2sem.server
 
+import com.prog2sem.shared.exceptions.InvalidUserInputException
+import com.prog2sem.shared.net.MsgMarker
+
 object Scheduler {
 
     fun listener() {
@@ -20,8 +23,17 @@ object Scheduler {
             }
         }
         SELECTOR.selectedKeys().clear()*/
-        val rec = SHEDULER.receive()
-        if (rec.isNotEmpty()) INVOKER.proceed(rec)
+        try {
+            val rec = SHEDULER.receive()
+            if (rec.isNotEmpty()) INVOKER.proceed(rec)
+        } catch (ex: Exception) {
+            when(ex) {
+                is IllegalArgumentException, is InvalidUserInputException -> {
+                    SHEDULER.send(MsgMarker.markGeneric("Пакеты повредились при транспортировке, пожалуйста попробуйте снова"))
+                }
+                else -> SHEDULER.send(MsgMarker.markGeneric("Ошибка соединения, пожалуйста попробуйте снова"))
+            }
+        }
     }
 
 /*    fun speaker() {
